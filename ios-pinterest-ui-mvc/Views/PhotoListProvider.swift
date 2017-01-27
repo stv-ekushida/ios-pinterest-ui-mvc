fe4f00746058dc5a2ec1b66ccdfbfd3bac5db480
Eiji Kushida <ekushida@EijinoMacBook-Pro.local>
2017-01-27 17:27:28 +0900

    Update PinterestLayoutDelegateをProviderに移動

--- ios-pinterest-ui-mvc/Views/PhotoListProvider.swift
+++ ios-pinterest-ui-mvc/Views/PhotoListProvider.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 final class PhotoListProvider: NSObject, UICollectionViewDataSource {
     
@@ -17,7 +18,7 @@ final class PhotoListProvider: NSObject, UICollectionViewDataSource {
     }
     
     func findByIndex(index: Int) -> Photo{
-        return self.photos[index]
+        return photos[index]
     }
     
     func collectionView(_ collectionView: UICollectionView,
@@ -32,5 +33,49 @@ final class PhotoListProvider: NSObject, UICollectionViewDataSource {
                                                       for: indexPath) as! PhotoListCell
         cell.photo = photos[indexPath.row]
         return cell
+    }
+}
+
+
+//MARK:- PinterestLayoutDelegate
+extension PhotoListProvider: PinterestLayoutDelegate {
+    
+    struct FontSize {
+        static let caption = CGFloat(13)
+        static let comment = CGFloat(11)
+    }
+    
+    struct Padding {
+        static let height = CGFloat(16)
+    }
+    
+    func collectionView(_ collectionView:UICollectionView,
+                        heightForPhotoAtIndexPath indexPath:IndexPath ,
+                        withWidth width:CGFloat) -> CGFloat {
+        
+        let boundingRect = CGRect(x: 0, y: 0,
+                                  width: width,
+                                  height: CGFloat(MAXFLOAT))
+        guard let image = photos[indexPath.row].image else{
+            return CGFloat(0)
+        }
+        
+        let rect = AVMakeRect(aspectRatio: image.size,
+                              insideRect: boundingRect)
+        return rect.size.height
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        heightForCaptionAndCommentAtIndexPath indexPath: IndexPath,
+                        withWidth width: CGFloat) -> CGFloat {
+        
+        let photo = photos[(indexPath as NSIndexPath).item]
+        
+        let captionrHeight = photo.heightForCaption(
+            UIFont.systemFont(ofSize: FontSize.caption), width: width)
+        let commentHeight = photo.heightForComment(
+            UIFont.systemFont(ofSize: FontSize.comment), width: width)
+        
+        return Padding.height + captionrHeight + commentHeight + Padding.height
     }
 }
